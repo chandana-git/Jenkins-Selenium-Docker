cdc4d02ee999da84f74c224a62ef5e7f19f25ebe
chandana-git <64481335+chandana-git@users.noreply.github.com>
2020-06-08 19:16:08 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		//System.setProperty("webdriver.chrome.driver", "C:/Users/chanrm/Downloads/chromedriver_win32/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/chanrm/Downloads/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		return driver;
 	}
